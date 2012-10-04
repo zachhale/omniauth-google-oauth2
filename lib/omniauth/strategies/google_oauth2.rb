b5523d672f35fdbad8faffb00d2fe9d42de9b57b
Zach Hale <zachhale@gmail.com>
2012-10-04 10:58:51 -0700

    Style improvements

--- lib/omniauth/strategies/google_oauth2.rb
+++ lib/omniauth/strategies/google_oauth2.rb
@@ -3,12 +3,19 @@ require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
     class GoogleOauth2 < OmniAuth::Strategies::OAuth2
-
       # Possible scopes: userinfo.email,userinfo.profile,plus.me
       DEFAULT_SCOPE = "userinfo.email,userinfo.profile"
 
+      @@authorize_options = [
+        :scope,
+        :approval_prompt,
+        :access_type,
+        :state,
+        :hd
+      ]
+
       option :name, 'google_oauth2'
-      option :authorize_options, [:scope, :approval_prompt, :access_type, :state, :hd]
+      option :authorize_options, @@authorize_options
 
       option :client_options, {
         :site          => 'https://accounts.google.com',
@@ -20,22 +27,25 @@ module OmniAuth
         base_scope_url = "https://www.googleapis.com/auth/"
         super.tap do |params|
           # Read the params if passed directly to omniauth_authorize_path
-          %w(scope approval_prompt access_type state hd).each do |k|
+          @@authorize_options.each do |k|
             params[k.to_sym] = request.params[k] unless [nil, ''].include?(request.params[k])
           end
+
           scopes = (params[:scope] || DEFAULT_SCOPE).split(",")
           scopes.map! { |s| s =~ /^https?:\/\// ? s : "#{base_scope_url}#{s}" }
           params[:scope] = scopes.join(' ')
+
           # This makes sure we get a refresh_token.
           # http://googlecode.blogspot.com/2011/10/upcoming-changes-to-oauth-20-endpoint.html
           params[:access_type] = 'offline' if params[:access_type].nil?
           params[:approval_prompt] = 'force' if params[:approval_prompt].nil?
+
           # Override the state per request
           session['omniauth.state'] = params[:state] if request.params['state']
         end
       end
 
-      uid{ raw_info['id'] || verified_email }
+      uid { raw_info['id'] || verified_email }
 
       info do
         prune!({
@@ -57,7 +67,7 @@ module OmniAuth
         @raw_info ||= access_token.get('https://www.googleapis.com/oauth2/v1/userinfo').parsed
       end
 
-      private
+    private
 
       def prune!(hash)
         hash.delete_if do |_, value|
@@ -69,7 +79,6 @@ module OmniAuth
       def verified_email
         raw_info['verified_email'] ? raw_info['email'] : nil
       end
-
     end
   end
 end
